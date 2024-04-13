e59f74eaa2bbb2c4a87cb01c8a4bea65db7d8155
Joe Farebrother <joseph.farebrother@gmail.com>
2024-04-13 20:38:29 +0100

    render fights with js

--- render.js
+++ render.js
@@ -31,3 +31,130 @@ window.setInterval(function () {
     ticks += 1
     drawClock(ticks)
 }, 1000)
+
+myName = "qaxnox"
+exampleFight = [
+    [
+        {
+            name: "qaxnox",
+            hp: 10,
+            power: 1,
+            human: true
+        },
+        {
+            name: "rixqexgoxqux",
+            hp: 10,
+            power: 2,
+            human: false
+        },
+        {
+            name: null,
+            hp: 10,
+            power: 2,
+            human: false
+        }
+    ],
+    [
+        {
+            name: null,
+            hp: 10,
+            power: 3,
+            human: true
+        },
+        {
+            name: null,
+            hp: 10,
+            power: 1,
+            human: false
+        },
+        {
+            name: null,
+            hp: 10,
+            power: 1,
+            human: false
+        },
+    ]
+
+]
+
+function renderDemon(demon) {
+    let elt = document.createElement("div")
+    elt.classList.add("demon")
+    if (demon.human) {
+        elt.classList.add("human")
+    }
+    if (demon.name == myName) {
+        elt.classList.add("you")
+    }
+
+    let nameElt = document.createElement("div")
+    nameElt.classList.add("demonName")
+    nameElt.textContent = demon.name || "???"
+    elt.appendChild(nameElt)
+
+    let imgElt = document.createElement("canvas")
+    imgElt.classList.add("demonImg")
+    renderDemonImg(demon, imgElt)
+    elt.appendChild(imgElt)
+
+    let hpElt = document.createElement("div")
+    hpElt.classList.add("demonHp")
+    hpElt.textContent = demon.hp + " HP"
+    elt.appendChild(hpElt)
+
+    let powerElt = document.createElement("div")
+    powerElt.classList.add("demonPower")
+    powerElt.textContent = demon.power
+    elt.appendChild(powerElt)
+
+    return elt
+}
+
+function renderDemonImg(demon, canvas) {
+    // TODO
+    canvas.width = 30
+    canvas.height = 30
+}
+
+function renderFightSide(side, left = true) {
+    let sideElt = document.createElement("div")
+    sideElt.classList.add("fightSide")
+    sideElt.classList.add(left ? "fightSideLeft" : "fightSideRight")
+
+    if (!left) {
+        side = side.map(i => i).reverse()
+    }
+
+    for (let demon of side) {
+        let demonElt = renderDemon(demon)
+        sideElt.appendChild(demonElt)
+    }
+
+    return sideElt
+}
+
+function renderFight(fight) {
+    let elt = document.createElement("div")
+    elt.classList.add("fight")
+
+    let leftSide = renderFightSide(fight[0], true)
+    elt.appendChild(leftSide)
+
+    let noMansLand = document.createElement("div")
+    noMansLand.classList.add("noMansLand")
+    elt.appendChild(noMansLand)
+
+    let rightSide = renderFightSide(fight[1], false)
+    elt.appendChild(rightSide)
+
+    return elt
+}
+
+function renderMainFight(fight) {
+    let mainFightElt = document.getElementById("mainFight")
+    mainFightElt.innerHTML = ""
+    mainFightElt.appendChild(renderFight(fight))
+}
+
+
+renderMainFight(exampleFight)
